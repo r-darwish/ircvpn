831dcdb065db461d0ceec8451d269f0e138524d4
Roey Darwish Dror <roey@toothless.local>
2014-07-26 19:38:47 +0300

    Using Boost Asio to read from the tunnel

--- src/irc_vpn.cpp
+++ src/irc_vpn.cpp
@@ -1,5 +1,46 @@
+#include <boost/bind.hpp>
+#include <boost/log/trivial.hpp>
+#include <sys/signal.h>
 #include "irc_vpn.hpp"
+
+using namespace boost::asio;
+
+void irc_vpn::on_quit_signal(
+    const boost::system::error_code & error,
+    int signal_number)
+{
+    if (error) {
+        BOOST_LOG_TRIVIAL(error) << "Signal Error";
+    } else {
+        BOOST_LOG_TRIVIAL(info) << "Caught signal " << signal_number;
+        io.stop();
+    }
+}
+
+void irc_vpn::on_tun_read(
+    const boost::system::error_code & error,
+    std::size_t bytes_read)
+{
+    if (error) {
+        BOOST_LOG_TRIVIAL(error) << "Error reading from the tunnel";
+    } else {
+        BOOST_LOG_TRIVIAL(debug) << "Read " << bytes_read << " from the tunnel";
+        tun_sd.async_read_some(
+            buffer(tun_read_buffer, sizeof(tun_read_buffer)),
+            boost::bind(&irc_vpn::on_tun_read, this, _1, _2));
+    }
+}
 
 void irc_vpn::run()
 {
+    signal_set quit_signals(io, SIGINT, SIGTERM, SIGQUIT);
+
+    quit_signals.async_wait(
+        boost::bind(&irc_vpn::on_quit_signal, this, _1, _2));
+
+    tun_sd.async_read_some(
+        buffer(tun_read_buffer, sizeof(tun_read_buffer)),
+        boost::bind(&irc_vpn::on_tun_read, this, _1, _2));
+
+    io.run();
 }
--- src/irc_vpn.hpp
+++ src/irc_vpn.hpp
@@ -1,18 +1,29 @@
 #ifndef __IRC_VPN_H__
 #define __IRC_VPN_H__
 
+#include <boost/asio.hpp>
 #include "tun_device.hpp"
+
+static const int TUN_READ_BUFFER_SIZE = 4096;
 
 class irc_vpn
 {
 public:
     irc_vpn(tun_device * tun) :
-        tun(tun)
+        tun_sd(io, tun->get_fd())
     { }
 
     void run();
 private:
-    tun_device * tun;
+    boost::asio::io_service io;
+    boost::asio::posix::stream_descriptor tun_sd;
+    char tun_read_buffer[TUN_READ_BUFFER_SIZE];
+    void on_tun_read(
+        const boost::system::error_code & error,
+        std::size_t bytes_read);
+    void on_quit_signal(
+        const boost::system::error_code & error,
+        int signal_number);
 };
 
 #endif
